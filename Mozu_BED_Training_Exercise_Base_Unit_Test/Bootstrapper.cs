4c4ffbee2f930b48eab7a88c129b29f2346ad1e6
allenmichael-volusion <allenmichaelvolusion@gmail.com>
2015-09-18 15:25:52 -0500

    Initial commit

---
+++ Mozu_BED_Training_Exercise_Base_Unit_Test/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Mozu.Api.ToolKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozu_BED_Training_Base_Unit_Test
+{
+    class Bootstrapper : AbstractBootstrapper
+    {
+    }
+}
---
+++ Mozu_BED_Training_Exercise_Base_Unit_Test/MozuDataConnectorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mozu.Api;
+using Autofac;
+using Mozu.Api.ToolKit.Config;
+using System.Collections.Generic;
+
+namespace Mozu_BED_Training_Base_Unit_Test
+{
+    [TestClass]
+    public class MozuDataConnectorTests
+    {
+        private IApiContext _apiContext;
+        private IContainer _container;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _container = new Bootstrapper().Bootstrap().Container;
+            var appSetting = _container.Resolve<IAppSetting>();
+            var tenantId = int.Parse(appSetting.Settings["TenantId"].ToString());
+            var siteId = int.Parse(appSetting.Settings["SiteId"].ToString());
+
+            _apiContext = new ApiContext(tenantId, siteId);
+        }
+
+        [TestMethod]
+        public void Exercise()
+        {
+        }
+    }
+}
