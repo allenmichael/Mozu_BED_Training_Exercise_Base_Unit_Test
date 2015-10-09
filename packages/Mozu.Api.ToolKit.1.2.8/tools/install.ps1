7f7e1cc4c5914f090ff869498089c69a089dcda1
allenmichael-volusion <allenmichaelvolusion@gmail.com>
2015-10-09 09:52:32 -0500

    Updated to Mozu SDK v1.18.1, revamped App.config, and deleted superfluous files

--- packages/Mozu.Api.ToolKit.1.2.8/tools/install.ps1
+++
@@ -1,4 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$item = $project.ProjectItems.Item("EmailTemplates").ProjectItems.Item("ErrorEmailTemplate.txt");
-$item.Properties.Item("CopyToOutputDirectory").Value = 1
